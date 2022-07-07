bf9e6eb19f48e726ab79e7163196f7757d37774d
Austin Gentz <austingentz@icloud.com>
2022-07-07 12:24:58 -0700

    styling and modal text

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,106 +2,104 @@
 M.AutoInit();
 
 //API Variables
-var monsterAPI = "https://www.dnd5eapi.co/api/monsters/"
-var classAPI = "https://www.dnd5eapi.co/api/classes/"
-var raceAPI = "https://www.dnd5eapi.co/api/classes/"
+var monsterAPI = 'https://www.dnd5eapi.co/api/monsters/';
+var classAPI = 'https://www.dnd5eapi.co/api/classes/';
+var raceAPI = 'https://www.dnd5eapi.co/api/classes/';
 
 //
-var classes = []
-var races = []
-var randomMonster = []
-var randomMonsterIndex = `${randomMonster}`
-var monsterStats = []
+var classes = [];
+var races = [];
+var randomMonster = [];
+var randomMonsterIndex = `${randomMonster}`;
+var monsterStats = [];
 
 //combat variables
 var hitRoll;
 var armorClass;
 var attackBonus;
-var attackBtn = document.querySelector("#attackTEST")
+var attackBtn = document.querySelector('#attackTEST');
 
 //buttons
-var characterGenBtn = document.querySelector("#new-character");
+var characterGenBtn = document.querySelector('#new-character');
 
 //elements
-var title = document.querySelector("a")
+var title = document.querySelector('a');
 
 //execute on page load
-    //classFetch
-    //racesFetch
+//classFetch
+//racesFetch
 
 //execute on combat load
-    //randomMonsterFetch
+//randomMonsterFetch
 
 //Get a class
-classFetch = function(){
-    fetch(classAPI) 
-    .then(function(response){
-        return response.json()
-    })
-    .then( function(data){
-        classes.push(data.results)
-        console.log(classes)
-    });
-}
-
+classFetch = function () {
+	fetch(classAPI)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			classes.push(data.results);
+			console.log(classes);
+		});
+};
 //Get a race
-racesFetch = function(){
-    fetch(raceAPI)
-    .then(function(response){
-        return response.json()
-    })
-    .then( function(data){
-
-        races.push(data.results)
-        console.log(races)
-    });
-}
+racesFetch = function () {
+	fetch(raceAPI)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			races.push(data.results);
+			console.log(races);
+		});
+};
 
 //Get a random monster
-    //another approach to this might be to see if we can generate a random number first, then use that to call a random monster from the API itself
-randomMonsterFetch = function() {
-    fetch(monsterAPI)
-    .then(function(response){
-        return response.json()
-    })
-    .then( function(data){
-        // console.log(data)
-        // console.log(Math.floor(Math.random() * 334))
-        randomMonster.push(data.results[Math.floor(Math.random() * 334)].index)
-        return randomMonster;
-    })
-    .then( function(){
-        var monsterStatApi = `https://www.dnd5eapi.co/api/monsters/${randomMonster}`
-        console.log(monsterStatApi)
-        fetch(monsterStatApi)
-        .then(function(response){
-        return response.json()
-     })
-    .then( function(monster){
-        // console.log(monster);
-        monsterStats.push(monster)
-        console.log(monsterStats)
-    });
-})
-}
+//another approach to this might be to see if we can generate a random number first, then use that to call a random monster from the API itself
+randomMonsterFetch = function () {
+	fetch(monsterAPI)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			// console.log(data)
+			// console.log(Math.floor(Math.random() * 334))
+			randomMonster.push(data.results[Math.floor(Math.random() * 334)].index);
+			return randomMonster;
+		})
+		.then(function () {
+			var monsterStatApi = `https://www.dnd5eapi.co/api/monsters/${randomMonster}`;
+			console.log(monsterStatApi);
+			fetch(monsterStatApi)
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (monster) {
+					// console.log(monster);
+					monsterStats.push(monster);
+					console.log(monsterStats);
+				});
+		});
+};
 
 randomMonsterFetch();
+classFetch();
+racesFetch();
 
 //combat functions
-function attackRoll(){
-
-    let attackBonus = 4
-    let armorClass = 6
-    var roll;
-    if (Math.floor(Math.random() * 20) === armorClass) { 
-        console.log("You hit!")
-    } else {
-        console.log("You missed!")
-        
-    }
+function attackRoll() {
+	let attackBonus = 4;
+	let armorClass = 6;
+	var roll;
+	if (Math.floor(Math.random() * 20) === armorClass) {
+		console.log('You hit!');
+	} else {
+		console.log('You missed!');
+	}
 }
 
-attackBtn.addEventListener("click", function (event){
-    event.preventDefault();
-    attackRoll();
-})
+attackBtn.addEventListener('click', function (event) {
+	event.preventDefault();
+	attackRoll();
+});
